643d407d88edebb2a97709452b9c47d79c6347d6
FelipeLafin <felahau@gmail.com>
2026-09-03 15:08:06 -0300

    Update API endpoint and fix token removal in dashboard script

--- frontend/src/js/dashboard.js
+++ frontend/src/js/dashboard.js
@@ -25,16 +25,18 @@ function atualizarDashboard() {
 }
 async function carregarFotoPerfilRestaurante() {
   try {
-    const response = await fetch("http://localhost:8080/restaurante", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
+    const response = await fetch("http://localhost:8081/tasty-app-bff/v1/restaurante/dashboard",
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
       },
-    });
+    );
 
     if (response.status === 401) {
-      localStorage.removeItem("token");
+      localStorage.removeItem("accessToken");
 
       window.location.href = "login.html";
 
@@ -56,7 +58,7 @@ async function carregarFotoPerfilRestaurante() {
 }
 
 function logout() {
-  localStorage.removeItem("token");
+  localStorage.removeItem("accessToken");
 
   window.location.href = "login.html";
 }
